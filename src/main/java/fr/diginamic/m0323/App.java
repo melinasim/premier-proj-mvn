e1a9762c9fbfe9a045b89bd32733e19c3a20e697
Simonnet Melina <melina.simonnet@wanadoo.fr>
2023-05-30 16:17:20 +0200

    project init

---
+++ src/main/java/fr/diginamic/m0323/App.java
@@ -0,0 +1,10 @@
+package fr.diginamic.m0323;
+import com.github.lalyos.jfiglet.FigletFont;
+
+public class App {
+            public static void main(String[] args) throws Exception {
+                String asciiArt = FigletFont.convertOneLine("App");
+                System.out.println(asciiArt);
+
+            }
+}
